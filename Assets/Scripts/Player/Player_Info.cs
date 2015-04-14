087bf9bcffc01a7e9ec30c4041fe9a9b427e4ca0
Tranklizer <trank101@mail.chapman.edu>
2015-04-13 20:04:25 -0700

    New Player controller and Testing room
    Since the default Unity First Person character controller can't detect
    collisions, I programmed my own. I also created a scene to test out it's
    movements

---
+++ Assets/Scripts/Player/Player_Info.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Info : MonoBehaviour {
+
+	public float moveSpeed;
+	public float turnSpeed;
+	public float maxAngle;
+	public float upThrust;
+	public int maxJumps;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
