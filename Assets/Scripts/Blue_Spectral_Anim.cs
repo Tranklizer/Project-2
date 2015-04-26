1e20cd6bfecabd4f9ed83407911c9e687ab98d12
freed108 <simonfreedman@Jasons-MBP.san.rr.com>
2015-04-26 14:27:38 -0700

    Blue_Spectral_Test
    I tried making the blue Spectrals. However, I had some problems making
    them. They will only lock on to the player if the player starts in
    their raycast zone. Also, I can’t get them to target the red Spectrals.

---
+++ Assets/Scripts/Blue_Spectral_Anim.cs
@@ -0,0 +1,182 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blue_Spectral_Anim : MonoBehaviour {
+
+	public Transform[] wanderPoints = new Transform[12];
+	public int currentWanderPoint = 0;
+	Transform target;
+	public GameObject RayCastPoint;
+	
+	public float moveSpeed;
+	public float turnSpeed;
+	
+	public int state = 0; //0 = wandering, 1 = attacking Player, 2 = attacking Spectral
+	
+	float attackTimer;
+	
+	//public Animator anim;
+	public GameObject player;
+	public GameObject Spectral;
+	
+	// Use this for initialization
+	void Start () {
+		target = wanderPoints [currentWanderPoint];
+		//anim = this.gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 relativePos = target.position - transform.position;
+		Quaternion turnRotation = Quaternion.LookRotation (relativePos);
+		transform.Translate (Vector3.forward * moveSpeed * Time.deltaTime);
+		transform.rotation = Quaternion.Slerp (transform.rotation, turnRotation, turnSpeed * Time.deltaTime);
+		
+		Vector3 forward = transform.TransformDirection(Vector3.forward);
+		
+		if (state == 0) //&& anim.GetCurrentAnimatorStateInfo(0).IsName("Wander"))
+		{
+			target = wanderPoints[currentWanderPoint];
+			
+			moveSpeed = 3;
+			turnSpeed = 1;
+			
+			/*RaycastHit hit;
+			Ray sight = new Ray(transform.position, forward);
+
+			if(Physics.Raycast(sight, out hit, 23.0f))
+			{
+				if (hit.collider.tag == "Player")
+				{
+					state = 1;
+					anim.SetTrigger("SeePlayer");
+					attackTimer = 0.0f;
+				}
+			}*/
+			
+			// Switch the target patrol point if the Spectral gets too close
+			if (Vector3.Distance (transform.position, target.position) <= 3) 
+			{
+				currentWanderPoint ++; //cycle through the wander point
+				if (currentWanderPoint >= wanderPoints.Length)  
+				{
+					currentWanderPoint = 0;
+				}
+				target = wanderPoints [currentWanderPoint];
+			}
+		}
+		
+		//if (state == 1 && anim.GetCurrentAnimatorStateInfo(0).IsName("Charge_WindUp"))
+		//{
+			//target = player.transform;
+			//moveSpeed = 1;
+		//}
+		
+		if (state == 1)
+		{
+			target = player.transform;
+			moveSpeed = 8;
+			turnSpeed = 3;
+			attackTimer += Time.deltaTime;
+			if (attackTimer > 3.0f)
+			{
+				state = 0;
+				//anim.SetTrigger("LostPlayer");
+				target = wanderPoints[currentWanderPoint];
+			}
+			else if (Vector3.Distance (transform.position, target.position) <= 8)
+			{
+				//anim.SetTrigger("AttackPlayer");
+				state = 0;
+			}
+		}
+
+		//if (state == 2 && anim.GetCurrentAnimatorStateInfo(0).IsName("Charge_WindUp"))
+		//{
+			//target = Spectral.transform;
+			//moveSpeed = 1;
+		//}
+		
+		if (state == 2)
+		{
+			target = Spectral.transform;
+			moveSpeed = 8;
+			turnSpeed = 3;
+			attackTimer += Time.deltaTime;
+			if (attackTimer > 3.0f)
+			{
+				state = 0;
+				//anim.SetTrigger("LostPlayer");
+				target = wanderPoints[currentWanderPoint];
+			}
+			else if (Vector3.Distance (transform.position, target.position) <= 8)
+			{
+				//anim.SetTrigger("AttackPlayer");
+				state = 0;
+			}
+		}
+		
+		//if (anim.GetCurrentAnimatorStateInfo (0).IsName ("Left_Swipe")) 
+		//{
+			//turnSpeed = 0;
+			//moveSpeed = 1;
+		//}
+		
+	}
+	
+	
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.tag == "Player") 
+		{
+			Debug.Log("Player is Dead");
+		}
+		if (other.gameObject.tag == "Building")
+		{
+			state = 0;
+		}
+		if (other.gameObject.tag == "Spectral") 
+		{
+			Debug.Log("Spectral is Dead");
+		}
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player") 
+		{
+			Vector3 relativePos = other.gameObject.transform.position - RayCastPoint.transform.position;
+			RaycastHit hit;
+			if(Physics.Raycast(RayCastPoint.transform.position, relativePos, out hit))
+			{
+				Debug.Log("Saw something");
+				Debug.Log(hit.collider.tag);
+				if(hit.collider.tag == "Player")
+				{
+					Debug.Log("Saw the player");
+					state = 1;
+					//anim.SetTrigger("SeePlayer");
+					attackTimer = 0.0f;
+				}
+			}
+		}
+
+		else if (other.gameObject.tag == "Spectral") 
+		{
+			Vector3 relativePos = other.gameObject.transform.position - RayCastPoint.transform.position;
+			RaycastHit hit;
+			if(Physics.Raycast(RayCastPoint.transform.position, relativePos, out hit))
+			{
+				Debug.Log("Saw something");
+				Debug.Log(hit.collider.tag);
+				if(hit.collider.tag == "Spectral")
+				{
+					Debug.Log("Saw the Spectral");
+					state = 2;
+					//anim.SetTrigger("SeePlayer");
+					attackTimer = 0.0f;
+				}
+			}
+		}
+	}
+}
