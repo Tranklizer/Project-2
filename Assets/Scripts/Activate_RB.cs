84060d6a52014105f2f4203e4db7122864110790
Tim Trankle <trank101@mail.chapman.edu>
2015-04-28 21:23:58 -0700

    In Class changes

---
+++ Assets/Scripts/Activate_RB.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Activate_RB : MonoBehaviour {
+	
+
+	// Use this for initialization
+	void Start () {
+		rigidbody.isKinematic = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if(other.gameObject.tag == "Player")
+		{
+			rigidbody.isKinematic = false;
+		}
+	}
+}
